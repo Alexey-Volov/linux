d581def2a75f4704cd8bba0c0e04719681b128b9
Alexey Volov <alexey.verstka@gmail.com>
2025-10-08 03:39:51 +0300

    v0.1 script du

---
+++ scripts/disk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -n "Type the directory: "
+read DIR
+if [[ -z $DIR ]]
+then
+	echo "Error! Type the directory!"
+	exit;
+else
+
+echo "-------------------------------"
+du -ahx $DIR | sort -rh | head
+echo "-------------------------------"
+fi
